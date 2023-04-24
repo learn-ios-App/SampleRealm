6f16b3b43e4b835e34eceac409f02ea06ed3434d
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-24 19:54:35 +0900

    Initial Commit

---
+++ SampleRealm/SampleRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleRealmApp.swift
+//  SampleRealm
+//
+//  Created by 渡邊魁優 on 2023/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
